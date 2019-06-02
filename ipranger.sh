e7efd0c5fcadffde25d168f2ae70801f4c06f16e
Vay3t <fffyanez@gmail.com>
2019-06-02 16:30:24 -0400

    Update ipranger.sh

--- ipranger.sh
+++ ipranger.sh
@@ -4,17 +4,17 @@ which curl &> /dev/null
 if [ $? -eq 0 ]; then
 	if [ $# -eq 1 ]; then
 		for asn in $(curl https://ipinfo.io/countries/$1 -s | grep "a href" | grep -iE "as[0-9]" | cut -d ">" -f3 | cut -d "<" -f1); do
-			echo "# $asn"
+			name=$(curl https://ipinfo.io/$asn -s | grep $asn | grep h3 | cut -d ">" -f2 | cut -d "<" -f1)
+			echo "# $name"
 			for iprange in $(curl https://ipinfo.io/$asn -s | grep $asn | grep -ioE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9]{1,2}\b"); do
 				echo $iprange
 			done
 			echo ""
 		done
 	else
-		echo "[*] Usage: bash $0 <country code>"
-		echo "Example: bash $0 us"
+		echo "[*] Usage: $0 <country code>"
+		echo "Example: $0 us"
 	fi
 else
 	echo "[-] please install curl"
 fi
-

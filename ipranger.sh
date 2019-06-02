8330d64131ac10f941ff5adf7909feda65c34b19
Vay3t <fffyanez@gmail.com>
2019-06-02 16:12:21 -0400

    Create ipranger.sh

---
+++ ipranger.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+which curl &> /dev/null
+if [ $? -eq 0 ]; then
+	if [ $# -eq 1 ]; then
+		for asn in $(curl https://ipinfo.io/countries/$1 -s | grep "a href" | grep -iE "as[0-9]" | cut -d ">" -f3 | cut -d "<" -f1); do
+			echo "# $asn"
+			for iprange in $(curl https://ipinfo.io/$asn -s | grep $asn | grep -ioE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9]{1,2}\b"); do
+				echo $iprange
+			done
+			echo ""
+		done
+	else
+		echo "[*] Usage: $0 <country code>"
+		echo "Example: $0 us"
+	fi
+else
+	echo "[-] please install curl"
+fi
+
